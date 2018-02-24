f23cc4cb9fbb7be2a322df19d857bbec3c540fdc
shiro <sunny@yui.org.cn>
2018-02-24 14:21:01 +0800

    add base components

--- src/components/base/index.js
+++ src/components/base/index.js
@@ -1,5 +1,15 @@
+import message from './message.js';
+import oSvg from './svg';
+import oSwitch from './switch';
+import oInput from './input';
+import oButton from './button';
 
 const install = function(Vue) {
+  Vue.prototype.$msg = message;
+  Vue.component('oSvg', oSvg);
+  Vue.component('oSwitch', oSwitch);
+  Vue.component('oInput', oInput);
+  Vue.component('oButton', oButton);
 };
 
 export default {
